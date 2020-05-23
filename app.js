029c862ac477c66c0d03530a1695a4980d02ad79
Emil Dolorfino <edolorfino24@gmail.com>
2020-05-23 06:26:55 -0400

    Level 2.5, environment variable memes

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
@@ -28,8 +29,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = "ThisIsALongSecretMessage";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = mongoose.model("User", userSchema);
 
