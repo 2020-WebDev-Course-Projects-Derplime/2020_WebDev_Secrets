bdb0311c8b43210b6a6eca16d53a40f75c44ff7c
Emil Dolorfino <edolorfino24@gmail.com>
2020-05-23 05:37:25 -0400

    startup done

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
-//jshint esversion:6
+//@ts-check
+
+const express = require("express");
+const ejs = require("ejs");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
